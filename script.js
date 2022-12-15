7b50bfd23d0ab2e3b1c2f04fde53fd30ae967690
Tawiwit <114454882+Tawiwit@users.noreply.github.com>
2022-12-15 19:07:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+var dataText = ["Daniel Lozano", " a Trash", "a College Dropped-out", "a Web Developer"];
+
+function typeWrite(text, i, fnCallback) {
+    if (i < text.length) {
+        document.querySelector("span").innerHTML = text.substring(0, i + 1) + "<span aria-hidden='true'></span>";
+        
+        setTimeout(function() {
+            typeWrite(text, i + 1, fnCallback);
+        }, 100);
+    }else if(typeof fnCallback == "function"){
+        setTimeout(fnCallback, 700);
+    }
+}
+
+function StartTextAnimation(i) {
+    if(typeof dataText[i] === "undefined" || dataText[i] === null) {
+        setTimeout(function() {
+            StartTextAnimation(0);
+        }, 5000);
+    }else{
+        if(i < dataText[i].length) {
+            typeWrite(dataText[i], 0, function(){
+                StartTextAnimation(i + 1);
+            })
+        }
+    }
+}
+
+StartTextAnimation(0);
+});
